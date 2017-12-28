e66a51e4085cfa425d5282a8ee99ff689ef32e17
nrifath2009 <Mahmuda@PLACOVU-WS-3>
2017-12-28 15:18:40 +0600

    Add project files.

---
+++ TestAutomationBuildApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestAutomationBuildApp.Startup))]
+namespace TestAutomationBuildApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
